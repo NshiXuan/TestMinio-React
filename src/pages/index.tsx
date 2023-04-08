20804b215dfb9337c61225000571f199cd67f56e
nsx <1743091664@qq.com>
2023-04-08 21:53:26 +0800

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,124 +1,94 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import { useState } from 'react';
+import { Md5 } from 'ts-md5/dist/md5';
 
 export default function Home() {
+  const [video, setVideo] = useState<any>()
+
+  // 定义每个分块为5M
+  const size = 5 * 1024 * 1024
+
+  let uploadUrl = "http://localhost:9001/upload/uploadchunk"
+  let checkUrl = "http://localhost:9001/upload/checkchunk"
+  let mergeUrl = "http://localhost:9001/upload/mergechunks"
+
+  const handleFileChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = event.target.files?.[0]
+    if (selectedFile) {
+      // 1.计算分块总数
+      const total = Math.ceil(selectedFile.size / size)
+
+      // 2.定义记录第几个分块
+      let chunkIndex = 0
+
+      // 3.定义文件的fileMd5
+      let fileMd5 = ''
+      // 获取第一个分片进而获取第一个分片的md5作为文件存储目录
+      const firstChunk = selectedFile.slice(0, size)
+
+      // 4.获取md5
+      const reader = new FileReader();
+      reader.onload = (event: any) => {
+        const arrayBuffer = event.target.result as ArrayBuffer;
+        // 将 ArrayBuffer 对象转换为字符串进行哈希
+        fileMd5 = Md5.hashStr(new TextDecoder().decode(arrayBuffer));
+      };
+
+      // 5.确定获取到fileMd5后再上传 最后合并
+      reader.onloadend = async function (event) {
+        for (let i = 0; i < selectedFile.size; i += size) {
+          // 5.1分块
+          const chunk = selectedFile.slice(i, i + size)
+
+          // 5.2检查分块是否已上传 
+          const checkForm = new FormData()
+          checkForm.append('fileMd5', fileMd5)
+          checkForm.append("chunkIndex", chunkIndex + "");
+          await fetch(checkUrl, { method: 'post', body: checkForm }).then(async res => {
+            const data = await res.json()
+
+            // 5.3如果没上传再上传
+            if (data.code == 0 && data.data != true) {
+              // 上传分块
+              const uploadForm = new FormData()
+              uploadForm.append('fileMd5', fileMd5)
+              uploadForm.append('chunk', chunk)
+              uploadForm.append("chunkIndex", chunkIndex + "");
+              await fetch(uploadUrl, { method: 'post', body: uploadForm })
+            }
+          })
+
+          // 5.4获取下个分块序号
+          chunkIndex++
+        }
+
+        // 5.5合并分块
+        const mergeForm = new FormData()
+        mergeForm.append('fileMd5', fileMd5)
+        mergeForm.append('fileName', selectedFile.name)
+        mergeForm.append("chunkTotal", total + "");
+        await fetch(mergeUrl, { method: 'post', body: mergeForm })
+      }
+      reader.readAsArrayBuffer(firstChunk);
+
+    }
+  }
+
+  // 获取视频
+  function handleButtonClick() {
+    fetch('http://localhost:9001/files').then(async res => {
+      const data = await res.json()
+      setVideo(data.data[data.data.length - 1])
+    })
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div>
+      <div>hello</div>
+      <input type="file" onChange={handleFileChange} />
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <button onClick={handleButtonClick}>获取视频</button>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      {video?.url && <video className='w-[80%]' src={video.url} controls></video>}
+    </div>
   )
 }
